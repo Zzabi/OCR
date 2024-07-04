ad2b6329d4d137ba141d0ac1a4935b435d2be2b4
Zzabi <100187147+Zzabi@users.noreply.github.com>
2024-07-04 21:23:22 +0530

    modified paths

--- realtime.py
+++ realtime.py
@@ -1,21 +1,28 @@
 import cv2
 from PIL import Image
 from pytesseract import pytesseract
+
+PATH_TO_TESSERACT = r"path\to\tesseract.exe"
+
 def tesseract():
-    path_to_tesseract = (r"C:\\Users\\my pc\\AppData\\Local\\Programs\\Tesseract-OCR\\tesseract.exe")
     image_path = "test1.jpg"
-    pytesseract.tesseract_cmd = path_to_tesseract
+    pytesseract.tesseract_cmd = PATH_TO_TESSERACT
     text = pytesseract.image_to_string(Image.open(image_path))
     print(text[:-1])
+
 camera=cv2.VideoCapture(0)
+
 while True:
     _,image=camera.read()
     cv2.imshow('image',image)
+    
     if cv2.waitKey(1)& 0xFF==ord('s' or 'S'):
         cv2.imwrite('test1.jpg',image)
         tesseract()
+        
     elif cv2.waitKey(1)& 0xFF==ord('q' or 'Q'):
         cv2.destroyAllWindows()
         break
+
 camera.release()
 cv2.destroyAllWindows()
--- video.py
+++ video.py
@@ -1,26 +1,33 @@
 import cv2
 import pytesseract
-import matplotlib as plt
-import numpy
-pytesseract.pytesseract.tesseract_cmd = (r"C:\\Users\\my pc\\AppData\\Local\\Programs\\Tesseract-OCR\\tesseract.exe")
+
+PATH_TO_TESSERACT = r"path\to\tesseract.exe"
+VIDEO_PATH = r"path\to\video.mp4"
+pytesseract.pytesseract.tesseract_cmd = (PATH_TO_TESSERACT)
+
 cv2.namedWindow("output", cv2.WINDOW_NORMAL)
 cv2.resizeWindow("output", 700, 900)
+
 font_scale = 1.5
 font = cv2.FONT_HERSHEY_PLAIN
-cap = cv2.VideoCapture("video.mp4")
+cap = cv2.VideoCapture(VIDEO_PATH)
+
 if not cap.isOpened():
     cap = cv2.VideoCapture(0)
 if not cap.isOpened():
     raise IOError("Cannot  open webcam")
+
 cntr = 0;
 while True:
     ret,frame = cap.read()
     cntr = cntr+1;
+
     if ((cntr%25)==0):
         imgH,imgW,_ = frame.shape
         x1,y1,w1,h1 = 0,0,imgH,imgW
         imgchar = pytesseract.image_to_string(frame)
         imgboxes = pytesseract.image_to_boxes(frame)
+
         for boxes in imgboxes.splitlines():
             boxes = boxes.split(' ')
             x,y,w,h = int(boxes[1]),int(boxes[2]),int(boxes[3]),int(boxes[4])
@@ -29,8 +36,10 @@ while True:
             print(imgchar)
             font = cv2.FONT_HERSHEY_SIMPLEX
             cv2.imshow("output",frame)
+
         if cv2.waitKey(1) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
             break
+
 cap.release()      
 cv2.destroyAllWindows()
